771601829e1c3d390185dc6ee86b929d165ff664
vibaprasath-ai <vibaprasath@gmail.com>
2025-11-01 17:16:54 +0530

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -2,7 +2,7 @@ arr=[1,27,52,63,17,78,42,10,72,58]
 print("\tBubble sort")
 print("\nBefore sorted:",arr)
 for i in range(0,len(arr)):
-    for i in range(0,len(arr)-1):
-        if arr[i]>arr[i+1]:
-            arr[i],arr[i+1]=arr[i+1],arr[i]
+    for j in range(0,len(arr)-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
 print("\nAfter sorted:",arr)
